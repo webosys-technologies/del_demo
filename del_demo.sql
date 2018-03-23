60e26a474e23ff3e29a3205a75d564fbe542a3c0
suraj_shinde\SURAJ <suraj@webosys.com>
2018-03-23 20:55:07 +0530

    del_demo database

---
+++ del_demo.sql
@@ -0,0 +1,651 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 23, 2018 at 10:39 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `del_demo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(20) NOT NULL,
+  `course_id` int(20) NOT NULL,
+  `book_name` varchar(40) NOT NULL,
+  `book_price` int(20) NOT NULL,
+  `book_status` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `centers`
+--
+
+CREATE TABLE `centers` (
+  `center_id` int(11) NOT NULL,
+  `center_fname` varchar(255) NOT NULL,
+  `center_lname` varchar(255) NOT NULL,
+  `center_name` varchar(255) NOT NULL,
+  `center_email` varchar(255) NOT NULL,
+  `center_mobile` varchar(12) NOT NULL,
+  `center_gender` varchar(11) NOT NULL,
+  `center_dob` date NOT NULL,
+  `center_profile_pic` varchar(255) NOT NULL,
+  `center_password` varchar(60) NOT NULL,
+  `center_verification` varchar(255) NOT NULL,
+  `center_address` varchar(255) NOT NULL,
+  `center_city` varchar(255) NOT NULL,
+  `center_pincode` int(11) NOT NULL,
+  `center_state` varchar(255) NOT NULL,
+  `center_created_at` date NOT NULL,
+  `center_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cities`
+--
+
+CREATE TABLE `cities` (
+  `city_id` int(11) NOT NULL,
+  `city_name` varchar(100) NOT NULL,
+  `city_state` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cities`
+--
+
+INSERT INTO `cities` (`city_id`, `city_name`, `city_state`) VALUES
+(1, 'Kolhapur', 'Maharashtra'),
+(783, 'Ahmednagar', 'Maharashtra'),
+(784, 'Akola', 'Maharashtra'),
+(785, 'Amravati', 'Maharashtra'),
+(786, 'Aurangabad', 'Maharashtra'),
+(787, 'Baramati', 'Maharashtra'),
+(788, 'Chalisgaon', 'Maharashtra'),
+(789, 'Chinchani', 'Maharashtra'),
+(790, 'Devgarh', 'Maharashtra'),
+(791, 'Dhule', 'Maharashtra'),
+(792, 'Dombivli', 'Maharashtra'),
+(793, 'Durgapur', 'Maharashtra'),
+(794, 'Ichalkaranji', 'Maharashtra'),
+(795, 'Jalna', 'Maharashtra'),
+(796, 'Kalyan', 'Maharashtra'),
+(797, 'Latur', 'Maharashtra'),
+(798, 'Loha', 'Maharashtra'),
+(799, 'Lonar', 'Maharashtra'),
+(800, 'Lonavla', 'Maharashtra'),
+(801, 'Mahad', 'Maharashtra'),
+(802, 'Mahuli', 'Maharashtra'),
+(803, 'Malegaon', 'Maharashtra'),
+(804, 'Malkapur', 'Maharashtra'),
+(805, 'Manchar', 'Maharashtra'),
+(806, 'Mangalvedhe', 'Maharashtra'),
+(807, 'Mangrulpir', 'Maharashtra'),
+(808, 'Manjlegaon', 'Maharashtra'),
+(809, 'Manmad', 'Maharashtra'),
+(810, 'Manwath', 'Maharashtra'),
+(811, 'Mehkar', 'Maharashtra'),
+(812, 'Mhaswad', 'Maharashtra'),
+(813, 'Miraj', 'Maharashtra'),
+(814, 'Morshi', 'Maharashtra'),
+(815, 'Mukhed', 'Maharashtra'),
+(816, 'Mul', 'Maharashtra'),
+(817, 'Mumbai', 'Maharashtra'),
+(818, 'Murtijapur', 'Maharashtra'),
+(819, 'Nagpur', 'Maharashtra'),
+(820, 'Nalasopara', 'Maharashtra'),
+(821, 'Nanded-Waghala', 'Maharashtra'),
+(822, 'Nandgaon', 'Maharashtra'),
+(823, 'Nandura', 'Maharashtra'),
+(824, 'Nandurbar', 'Maharashtra'),
+(825, 'Narkhed', 'Maharashtra'),
+(826, 'Nashik', 'Maharashtra'),
+(827, 'Navi Mumbai', 'Maharashtra'),
+(828, 'Nawapur', 'Maharashtra'),
+(829, 'Nilanga', 'Maharashtra'),
+(830, 'Osmanabad', 'Maharashtra'),
+(831, 'Ozar', 'Maharashtra'),
+(832, 'Pachora', 'Maharashtra'),
+(833, 'Paithan', 'Maharashtra'),
+(834, 'Palghar', 'Maharashtra'),
+(835, 'Pandharkaoda', 'Maharashtra'),
+(836, 'Pandharpur', 'Maharashtra'),
+(837, 'Panvel', 'Maharashtra'),
+(838, 'Parbhani', 'Maharashtra'),
+(839, 'Parli', 'Maharashtra'),
+(840, 'Parola', 'Maharashtra'),
+(841, 'Partur', 'Maharashtra'),
+(842, 'Pathardi', 'Maharashtra'),
+(843, 'Pathri', 'Maharashtra'),
+(844, 'Patur', 'Maharashtra'),
+(845, 'Pauni', 'Maharashtra'),
+(846, 'Pen', 'Maharashtra'),
+(847, 'Phaltan', 'Maharashtra'),
+(848, 'Pulgaon', 'Maharashtra'),
+(849, 'Pune', 'Maharashtra'),
+(850, 'Purna', 'Maharashtra'),
+(851, 'Pusad', 'Maharashtra'),
+(852, 'Rahuri', 'Maharashtra'),
+(853, 'Rajura', 'Maharashtra'),
+(854, 'Ramtek', 'Maharashtra'),
+(855, 'Ratnagiri', 'Maharashtra'),
+(856, 'Raver', 'Maharashtra'),
+(857, 'Risod', 'Maharashtra'),
+(858, 'Sailu', 'Maharashtra'),
+(859, 'Sangamner', 'Maharashtra'),
+(860, 'Sangli', 'Maharashtra'),
+(861, 'Sangole', 'Maharashtra'),
+(862, 'Sasvad', 'Maharashtra'),
+(863, 'Satana', 'Maharashtra'),
+(864, 'Satara', 'Maharashtra'),
+(865, 'Savner', 'Maharashtra'),
+(866, 'Sawantwadi', 'Maharashtra'),
+(867, 'Shahade', 'Maharashtra'),
+(868, 'Shegaon', 'Maharashtra'),
+(869, 'Shendurjana', 'Maharashtra'),
+(870, 'Shirdi', 'Maharashtra'),
+(871, 'Shirpur-Warwade', 'Maharashtra'),
+(872, 'Shirur', 'Maharashtra'),
+(873, 'Shrigonda', 'Maharashtra'),
+(874, 'Shrirampur', 'Maharashtra'),
+(875, 'Sillod', 'Maharashtra'),
+(876, 'Sinnar', 'Maharashtra'),
+(877, 'Solapur', 'Maharashtra'),
+(878, 'Soyagaon', 'Maharashtra'),
+(879, 'Talegaon Dabhade', 'Maharashtra'),
+(880, 'Talode', 'Maharashtra'),
+(881, 'Tasgaon', 'Maharashtra'),
+(882, 'Tirora', 'Maharashtra'),
+(883, 'Tuljapur', 'Maharashtra'),
+(884, 'Tumsar', 'Maharashtra'),
+(885, 'Uran', 'Maharashtra'),
+(886, 'Uran Islampur', 'Maharashtra'),
+(887, 'Wadgaon Road', 'Maharashtra'),
+(888, 'Wai', 'Maharashtra'),
+(889, 'Wani', 'Maharashtra'),
+(890, 'Wardha', 'Maharashtra'),
+(891, 'Warora', 'Maharashtra'),
+(892, 'Warud', 'Maharashtra'),
+(893, 'Washim', 'Maharashtra'),
+(894, 'Yevla', 'Maharashtra'),
+(895, 'Uchgaon', 'Maharashtra'),
+(896, 'Udgir', 'Maharashtra'),
+(897, 'Umarga', 'Maharastra'),
+(898, 'Umarkhed', 'Maharastra'),
+(899, 'Umred', 'Maharastra'),
+(900, 'Vadgaon Kasba', 'Maharastra'),
+(901, 'Vaijapur', 'Maharastra'),
+(902, 'Vasai', 'Maharastra'),
+(903, 'Virar', 'Maharastra'),
+(904, 'Vita', 'Maharastra'),
+(905, 'Yavatmal', 'Maharastra'),
+(906, 'Yawal', 'Maharastra');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(255) NOT NULL,
+  `course_duration` float NOT NULL,
+  `course_fees` int(11) NOT NULL,
+  `course_reexam_fees` int(11) NOT NULL,
+  `course_created_at` date NOT NULL,
+  `course_created_by` varchar(100) NOT NULL,
+  `course_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exams`
+--
+
+CREATE TABLE `exams` (
+  `exam_id` int(11) NOT NULL,
+  `question_paper_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `exam_type` varchar(200) NOT NULL,
+  `exam_obtain_marks` int(11) NOT NULL,
+  `exam_taken_time` varchar(100) NOT NULL,
+  `exam_percentage` float NOT NULL,
+  `exam_result` varchar(100) NOT NULL,
+  `exam_date` datetime NOT NULL,
+  `exam_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam_details`
+--
+
+CREATE TABLE `exam_details` (
+  `exam_detail_id` int(11) NOT NULL,
+  `exam_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `correct_ans` varchar(255) NOT NULL,
+  `given_ans` varchar(255) NOT NULL,
+  `mark` int(11) NOT NULL,
+  `taken_time` varchar(50) NOT NULL,
+  `ans_status` varchar(100) NOT NULL,
+  `exam_detail_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `center_id` int(11) NOT NULL,
+  `order_name` varchar(255) NOT NULL,
+  `order_amount` int(11) NOT NULL,
+  `student_qty` int(11) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_status` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `order_detail_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `center_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `od_course_fees` int(11) NOT NULL,
+  `od_book_price` int(20) NOT NULL,
+  `od_total_amount` int(20) NOT NULL,
+  `order_detail_status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `p_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `payment_id` int(11) NOT NULL,
+  `merchant_order_id` varchar(60) NOT NULL,
+  `payment_gateway` varchar(60) NOT NULL,
+  `bank_ref_num` varchar(60) NOT NULL,
+  `center_id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `mobile` varchar(60) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `payment_at` date NOT NULL,
+  `payment_status` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `play_time`
+--
+
+CREATE TABLE `play_time` (
+  `play_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `played` int(11) NOT NULL,
+  `remaining_play_time` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `question_name` varchar(255) NOT NULL,
+  `question_option_a` varchar(255) NOT NULL,
+  `question_option_b` varchar(255) NOT NULL,
+  `question_option_c` varchar(255) NOT NULL,
+  `question_option_d` varchar(255) NOT NULL,
+  `question_correct_ans` varchar(50) NOT NULL,
+  `question_created_at` date NOT NULL,
+  `question_created_by` varchar(100) NOT NULL,
+  `question_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `question_papers`
+--
+
+CREATE TABLE `question_papers` (
+  `question_paper_id` int(11) NOT NULL,
+  `qp_name` varchar(255) NOT NULL,
+  `qp_no_of_qsn` int(11) NOT NULL,
+  `qp_total_marks` int(11) NOT NULL,
+  `qp_total_time` varchar(255) NOT NULL,
+  `qp_created_at` date NOT NULL,
+  `qp_created_by` varchar(100) NOT NULL,
+  `qp_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `center_id` int(11) NOT NULL,
+  `course_id` int(60) NOT NULL,
+  `student_book` int(20) NOT NULL,
+  `student_fname` varchar(200) NOT NULL,
+  `student_lname` varchar(250) NOT NULL,
+  `student_email` varchar(200) NOT NULL,
+  `student_mobile` varchar(12) NOT NULL,
+  `student_gender` varchar(50) NOT NULL,
+  `student_dob` date NOT NULL,
+  `student_last_education` varchar(255) NOT NULL,
+  `student_address` text NOT NULL,
+  `student_city` varchar(255) NOT NULL,
+  `student_state` varchar(255) NOT NULL,
+  `student_pincode` int(11) NOT NULL,
+  `student_username` varchar(255) NOT NULL,
+  `student_password` varchar(255) NOT NULL,
+  `student_exam_passcode` varchar(255) NOT NULL,
+  `student_profile_pic` varchar(255) NOT NULL,
+  `student_created_at` date NOT NULL,
+  `student_admission_month` varchar(250) NOT NULL,
+  `student_admission_date` date NOT NULL,
+  `student_course_start_date` date NOT NULL,
+  `student_course_end_date` date NOT NULL,
+  `student_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topics`
+--
+
+CREATE TABLE `topics` (
+  `topic_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `topic_name` varchar(255) NOT NULL,
+  `topic_video_path` varchar(250) NOT NULL,
+  `topic_description` text NOT NULL,
+  `topic_video_play_time` int(11) NOT NULL,
+  `topic_created_at` date NOT NULL,
+  `topic_created_by` varchar(100) NOT NULL,
+  `topic_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `user_fname` varchar(255) NOT NULL,
+  `user_lname` varchar(255) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_profile_pic` varchar(255) NOT NULL,
+  `user_mobile` varchar(20) NOT NULL,
+  `user_gender` varchar(50) NOT NULL,
+  `user_password` int(11) NOT NULL,
+  `user_created_at` date NOT NULL,
+  `user_type` varchar(11) NOT NULL,
+  `user_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `user_fname`, `user_lname`, `user_email`, `user_profile_pic`, `user_mobile`, `user_gender`, `user_password`, `user_created_at`, `user_type`, `user_status`) VALUES
+(1, 'SUNIL', 'KHANDARE', 'admin@gmail.com', '', '9874563210', 'Male', 12345678, '2018-03-23', 'admin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `video_id` int(11) NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL,
+  `video_name` varchar(255) NOT NULL,
+  `video_path` varchar(255) NOT NULL,
+  `video_play_time` int(11) NOT NULL,
+  `video_created_at` date NOT NULL,
+  `video_created_by` varchar(100) NOT NULL,
+  `video_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+
+--
+-- Indexes for table `centers`
+--
+ALTER TABLE `centers`
+  ADD PRIMARY KEY (`center_id`);
+
+--
+-- Indexes for table `cities`
+--
+ALTER TABLE `cities`
+  ADD PRIMARY KEY (`city_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `exams`
+--
+ALTER TABLE `exams`
+  ADD PRIMARY KEY (`exam_id`);
+
+--
+-- Indexes for table `exam_details`
+--
+ALTER TABLE `exam_details`
+  ADD PRIMARY KEY (`exam_detail_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`order_detail_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`p_id`);
+
+--
+-- Indexes for table `play_time`
+--
+ALTER TABLE `play_time`
+  ADD PRIMARY KEY (`play_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `question_papers`
+--
+ALTER TABLE `question_papers`
+  ADD PRIMARY KEY (`question_paper_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`topic_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`video_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `centers`
+--
+ALTER TABLE `centers`
+  MODIFY `center_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `cities`
+--
+ALTER TABLE `cities`
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=907;
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `exams`
+--
+ALTER TABLE `exams`
+  MODIFY `exam_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `exam_details`
+--
+ALTER TABLE `exam_details`
+  MODIFY `exam_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `order_detail_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `play_time`
+--
+ALTER TABLE `play_time`
+  MODIFY `play_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `question_papers`
+--
+ALTER TABLE `question_papers`
+  MODIFY `question_paper_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `topic_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
