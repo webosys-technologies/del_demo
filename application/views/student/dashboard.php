a90643cc0e08d89c03bad5c055301e2a01c31f15
suraj_shinde\SURAJ <suraj@webosys.com>
2018-03-29 10:54:20 +0530

    calender view in student dashboard

--- application/views/student/dashboard.php
+++ application/views/student/dashboard.php
@@ -234,8 +234,8 @@
               <div class="pull-right box-tools">
                 <!-- button with a dropdown -->
                 <div class="btn-group">
-                  <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
-                    <i class="fa fa-bars"></i></button>
+<!--                  <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown">
+                    <i class="fa fa-bars"></i></button>-->
                   <ul class="dropdown-menu pull-right" role="menu">
                     <li><a href="#">Add new event</a></li>
                     <li><a href="#">Clear events</a></li>
@@ -503,45 +503,45 @@
   <!-- /.control-sidebar -->
   <!-- Add the sidebar's background. This div must be placed
        immediately after the control sidebar -->
-<!--  <div class="control-sidebar-bg"></div>
- ./wrapper 
+  <!--<div class="control-sidebar-bg"></div>-->
+ <!--./wrapper--> 
   
- jQuery 3 
-<script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
- jQuery UI 1.11.4 
+ <!--jQuery 3--> 
+<!--<script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>-->
+ <!--jQuery UI 1.11.4--> 
 <script src="<?php echo base_url(); ?>assets/bower_components/jquery-ui/jquery-ui.min.js"></script>
- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip 
-<script>
+ <!--Resolve conflict in jQuery UI tooltip with Bootstrap tooltip--> 
+<!--<script>
   $.widget.bridge('uibutton', $.ui.button);
-</script>
- Bootstrap 3.3.7 
-<script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
- Morris.js charts 
-<script src="<?php echo base_url(); ?>assets/bower_components/raphael/raphael.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/bower_components/morris.js/morris.min.js"></script>
- Sparkline 
+</script>-->
+ <!--Bootstrap 3.3.7--> 
+<!--<script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>-->
+ <!--Morris.js charts--> 
+<!--<script src="<?php echo base_url(); ?>assets/bower_components/raphael/raphael.min.js"></script>-->
+<!--<script src="<?php echo base_url(); ?>assets/bower_components/morris.js/morris.min.js"></script>-->
+ <!--Sparkline--> 
 <script src="<?php echo base_url(); ?>assets/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
- jvectormap 
+ <!--jvectormap--> 
 <script src="<?php echo base_url(); ?>assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
- jQuery Knob Chart 
+ <!--jQuery Knob Chart--> 
 <script src="<?php echo base_url(); ?>assets/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
- daterangepicker 
+ <!--daterangepicker--> 
 <script src="<?php echo base_url(); ?>assets/bower_components/moment/min/moment.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
- datepicker 
+ <!--datepicker--> 
 <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
- Bootstrap WYSIHTML5 
+ <!--Bootstrap WYSIHTML5--> 
 <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
- Slimscroll 
-<script src="<?php echo base_url(); ?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
- FastClick 
-<script src="<?php echo base_url(); ?>assets/bower_components/fastclick/lib/fastclick.js"></script>
+ <!--Slimscroll--> 
+<!--<script src="<?php echo base_url(); ?>assets/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>-->
+ <!--FastClick--> 
+<!--<script src="<?php echo base_url(); ?>assets/bower_components/fastclick/lib/fastclick.js"></script>
  AdminLTE App 
-<script src="<?php echo base_url(); ?>assets/dist/js/adminlte.min.js"></script>
- AdminLTE dashboard demo (This is only for demo purposes) 
+<script src="<?php echo base_url(); ?>assets/dist/js/adminlte.min.js"></script>-->
+ <!--AdminLTE dashboard demo (This is only for demo purposes)--> 
 <script src="<?php echo base_url(); ?>assets/dist/js/pages/dashboard.js"></script>
- AdminLTE for demo purposes 
-<script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>
-</body>
+ <!--AdminLTE for demo purposes--> 
+<!--<script src="<?php echo base_url(); ?>assets/dist/js/demo.js"></script>-->
+<!--</body>
 </html>-->
