5412ff603ba199a467872174e1ada79551de5fba
suraj_shinde\SURAJ <suraj@webosys.com>
2018-06-09 19:35:50 +0530

    demo changes

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -11,6 +11,7 @@
     <!-- <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script> -->
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <!-- Bootstrap 3.3.2 JS -->
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap-auto-dismiss-alert.js"></script>
     <script src="<?php echo base_url(); ?>assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
     <script src="<?php echo base_url(); ?>assets/dist/js/app.min.js" type="text/javascript"></script>
     <script src="<?php echo base_url(); ?>assets/js/jquery.validate.js" type="text/javascript"></script>
